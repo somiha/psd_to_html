7147cab214057793e936f8ffb35f3167af1057f2
Somiha <stasnimjes@gmail.com>
2021-06-15 21:03:17 +0600

    first commit

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+jQuery(document).ready(()=>{
+    jQuery('.slider_area').bxSlider();
+    jQuery('#main_menu').meanmenu({
+        meanMenuContainer: '#mobile_menu',
+        meanScreenWidth: 991,
+    });
+});
